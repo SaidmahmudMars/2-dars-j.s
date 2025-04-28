61a3114ceae05fe83f21d4fedecb7b1518ac9237
SaidmahmudMars <abdusamatovsaidmahmud@gmail.com>
2025-04-28 20:24:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const userName = prompt("String");
+
+alert(`${userName} sozi : ${typeof userName}`);
+const hayokiyoq = prompt("Boolean Kiriting");
+
+console.log(`${hayokiyoq} sozi: ${typeof Boolean(Saidmaxmud)}`);
+const Qw = prompt("String");
+
+alert(`${Saidmaxmud} sozi : ${typeof userName}`);
+const Saidmaxmud = prompt("Boolean Kiriting");
+
+console.log(`${Saidmaxmud} sozi: ${typeof Boolean(Saidmaxmud)}`);
+
++Number('45216789348765267890n');
+console.log(userName);
